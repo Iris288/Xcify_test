3a2137286a067013b2f10e24ea61dbb8f1458a01
Iris288 <101178777+Iris288@users.noreply.github.com>
2023-06-08 15:23:09 +1000

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XcifyTestCode.Models;
+
+
+namespace XcifyTestCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private XcifySampleTestDB objProductDbEntities;
+
+        public HomeController()
+        {
+            objProductDbEntities = new XcifySampleTestDB();
+        }
+        public ActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public JsonResult GetAllProduct()
+        {
+            var ProductGet = (from obj in objProductDbEntities.tblProducts select new
+                                {
+                                    Name = obj.Name,
+                                    Price = obj.Price
+                                }
+
+                            ).ToList();
+            return Json(ProductGet, JsonRequestBehavior.AllowGet);
+
+            
+        }
+    }
+}
---
+++ Models/ProductDataContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+
+namespace XcifyTestCode.Models
+{
+    
+    public partial class XcifySampleTestDB : DataContext
+    {
+        public XcifySampleTestDB() : base("Data Source=XcifySampleTestDB;Initial Catalog=XcifySampleTestDB;Integrated Security=True")
+        {
+        }
+
+
+        //public Table<tblProduct> Products => GetTable<tblProduct>();
+        public virtual Table<tblProduct> tblProducts { get; set; }
+    }
+
+    
+}
